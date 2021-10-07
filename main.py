59b5a89b4890d623abf61d1e2ceffc26f6ce32e6
ekozthon <92107110+ekozthon@users.noreply.github.com>
2021-10-08 02:09:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import os
-os.system("git clone https://github.com/ekozthon/DARKEN/sessionhack_bot")
+os.system("git clone https://github.com/ekozthon/sessionhack_bot/sessionhack_bot")
 os.system("cd sessionhack_bot && python bot.py")
